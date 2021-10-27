f496df0e062254f512a593bbe871e9e03190d237
DenisButmalay <denis.butmalay@yande.ru>
2021-10-28 02:46:34 +0300

    Initial Commit

---
+++ Who_wants_to_be_a_millioner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Who_wants_to_be_a_millioner
+//
+//  Created by Денис Бутмалай on 28.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
